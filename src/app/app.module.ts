d757dd84e3a87dc5f6926e7ab94161dcc26a3922
Gaurav patidar <gp26101998@gmail.com>
2020-12-23 17:32:23 +0530

    first aapko template design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { ComplexComponent } from './complex/complex.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SayComponent } from './say/say.component';
+import { WatchComponent } from './watch/watch.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import {OwlModule} from 'ngx-owl-carousel';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShowcaseComponent,
+    ComplexComponent,
+    FeaturesComponent,
+    PricingComponent,
+    SayComponent,
+    WatchComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
